0c7c862fac97237faf25e8afced5c38cf5a2fd9a
Frank van de Voorde <daemonaz@kymp.net>
2023-02-13 16:22:14 +0200

    Change the header and top of nav menu to be fixed

--- portfolio-project/src/routes/skills.tsx
+++ portfolio-project/src/routes/skills.tsx
@@ -5,6 +5,12 @@ const Skills: FunctionComponent = () => {
 	return (
 		<main id="skills">
 			<Header>Skills</Header>
+			<section className="skill-list">
+				<h3 className="large-header">The skills and tools I use</h3>
+				<div className="skill-list skills">
+
+				</div>
+			</section>
 		</main>
 	);
 }
