89ca095c0de633db81bf0c8fb739c83217db9dda
Frank van de Voorde <daemonaz@kymp.net>
2023-02-16 16:24:01 +0200

    Add page animations

--- portfolio-project/src/routes/root.tsx
+++ portfolio-project/src/routes/root.tsx
@@ -1,13 +1,22 @@
 import React, { FunctionComponent } from 'react';
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
+import { motion } from "framer-motion"
 import Navbar from '../components/navbar/navbar';
 import './root.scss';
 
 const Root: FunctionComponent = () => {
+	const { pathname } = useLocation();
 	return (
 		<div id="page">
 			<Navbar />
-			<Outlet />
+			<motion.div
+		        key={pathname}
+		        initial={{ opacity: 0, scale: 0.7 }}
+			    animate={{ opacity: 1, scale: 1 }}
+			    transition={{ duration: 0.5 }}
+	        >
+				<Outlet />
+			</motion.div>
 		</div>
 	)
 }
