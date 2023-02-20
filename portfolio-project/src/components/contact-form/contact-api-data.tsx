c2d5d2f067e2aae98e4feb419a5999062236a1cc
Frank van de Voorde <daemonaz@kymp.net>
2023-02-20 15:28:27 +0200

    Remove api data

---
+++ portfolio-project/src/components/contact-form/contact-api-data.tsx
@@ -0,0 +1,13 @@
+interface ApiData {
+	service: string;
+	template: string;
+	key: string;
+};
+
+const apiData = {
+	service: 'service_t9n6o9m',
+	template: 'template_ebm37pp',
+	key: 'IO_5P-XgKIY9jWUYR'
+}
+
+export default apiData;
